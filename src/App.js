8e31d24372d5be6fff2a3fc45c09684f4f8db2a0
최철환 <hwanyhee@naver.com>
2019-05-25 14:59:09 +0900

    git push/pull test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const players = [
   {name: 'LDK', score: 30, id: 1},
   {name: 'HONG', score: 40, id: 2},
   {name: 'KIM', score: 50, id: 3},
-  {name: 'PARK', score: 60, id: 4},
+  {name: 'PARK', score: 70, id: 4},
 ];
 
 const Header = (props) => {
