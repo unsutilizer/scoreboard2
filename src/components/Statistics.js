248ab2cf6953694c3637be93599ca2830a33ab7c
최철환 <hwanyhee@naver.com>
2019-05-25 16:39:52 +0900

    Statistics 컴포넌트 로직 구현

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 export const Statistics = (props) => {
+  const totalPlayers = props.players.length;
+  let totalScore = 0;
+  props.players.forEach(item => totalScore += item.score)
+
   return (
     <table>
       <tbody>
       <tr>
         <td>Players:</td>
-        <td>0</td>
+        <td>{totalPlayers}</td>
       </tr>
       <tr>
         <td>Total Score:</td>
-        <td>0</td>
+        <td>{totalScore}</td>
       </tr>
       </tbody>
     </table>
