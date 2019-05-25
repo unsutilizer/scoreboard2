058fd1d55165a1d95eead103dcf9e9e9b2e85ae0
최철환 <hwanyhee@naver.com>
2019-05-25 16:58:38 +0900

    AddPlayerForm 을 controlled component로 만들기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Player} from "./components/Player";
+import {AddPlayerForm} from "./components/AddPlayerForm";
 
 class App extends React.Component {
   state = {
@@ -51,6 +52,7 @@ class App extends React.Component {
                     changeScore={this.handleChangeScore} />
           ))
         }
+        <AddPlayerForm/>
       </div>
     );
   }
